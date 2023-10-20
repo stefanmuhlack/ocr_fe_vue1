9d11e4d95200e6f1e713fafd4e426d6273141bf8
Stefan Muhlack <stefan.muhlack@wwe-media.eu>
2023-10-20 11:43:36 +0200

    init 2

--- src/api.js
+++ src/api.js
@@ -2,11 +2,19 @@
 
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000'; // Replace with your backend API URL
+const baseURL = process.env.VUE_APP_BACKEND_URL || 'http://localhost:8000';
+
 
 const api = axios.create({
     baseURL,
     timeout: 5000, // Adjust as needed
 });
-
 export default api;
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        console.error("API request failed:", error);
+        return Promise.reject(error);
+    }
+);
