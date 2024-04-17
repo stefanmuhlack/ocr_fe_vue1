ac2c216c1bfb6c38008dd26113a51051a6cdd7d4
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-04-17 13:59:40 +0200

    Enhance UploadForm with real-time feedback and improved UI.

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="upload-form">
+    <h1>Upload Your PDF</h1>
     <input type="file" @change="handleFileUpload" />
     <button @click="submitFile">Upload File</button>
+    <p v-if="uploadStatus">{{ uploadStatus }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      file: null,
+      uploadStatus: ''
+    };
+  },
   methods: {
     handleFileUpload(event) {
       this.file = event.target.files[0];
+      this.uploadStatus = 'File ready for upload';
     },
     submitFile() {
       this.$emit('uploadPDF', this.file);
+      this.uploadStatus = 'Uploading...';
+      // Add more logic here for real-time feedback using WebSockets
+    }
+  }
+};
+</script>
+
     }
   }
 };
