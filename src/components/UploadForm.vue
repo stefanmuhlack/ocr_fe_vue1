0944ebfc7fc8b21ee04f94af36df5530d3435f2a
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-04-17 13:47:11 +0200

    Create UploadForm component for handling file uploads.

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="upload-form">
+    <input type="file" @change="handleFileUpload" />
+    <button @click="submitFile">Upload File</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleFileUpload(event) {
+      this.file = event.target.files[0];
+    },
+    submitFile() {
+      this.$emit('uploadPDF', this.file);
+    }
+  }
+};
+</script>
