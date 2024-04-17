3f03f8364d6f366899bfac6f6c28498362d3d677
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-04-17 15:03:52 +0200

    Enhance UploadForm with progress bar and improved UX.

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -3,6 +3,7 @@
     <h1>Upload Your PDF</h1>
     <input type="file" @change="handleFileUpload" />
     <button @click="submitFile">Upload File</button>
+    <progress v-if="uploading" max="100" :value="uploadProgress"></progress>
     <p v-if="uploadStatus">{{ uploadStatus }}</p>
   </div>
 </template>
@@ -12,7 +13,9 @@ export default {
   data() {
     return {
       file: null,
-      uploadStatus: ''
+      uploadStatus: '',
+      uploading: false,
+      uploadProgress: 0
     };
   },
   methods: {
@@ -21,15 +24,31 @@ export default {
       this.uploadStatus = 'File ready for upload';
     },
     submitFile() {
-      this.$emit('uploadPDF', this.file);
+      this.uploading = true;
       this.uploadStatus = 'Uploading...';
-      // Add more logic here for real-time feedback using WebSockets
+      // Simulate upload progress
+      const interval = setInterval(() => {
+        if (this.uploadProgress < 100) {
+          this.uploadProgress += 10;
+        } else {
+          clearInterval(interval);
+          this.uploadStatus = 'Upload Complete';
+          this.uploading = false;
+        }
+      }, 300);
     }
   }
 };
 </script>
 
-    }
-  }
-};
-</script>
+<style scoped>
+.upload-form {
+  text-align: center;
+  margin-top: 20px;
+}
+progress {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
+
