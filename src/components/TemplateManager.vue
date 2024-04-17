524b3b4137551c75f5e72a18ec4dff5f0a68d53b
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-04-17 13:48:54 +0200

    Create TemplateManager component for managing OCR templates.

---
+++ src/components/TemplateManager.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="template-manager">
+    <h2>Manage Templates</h2>
+    <div v-for="template in templates" :key="template.id">
+      <p>{{ template.name }}</p>
+      <button @click="editTemplate(template.id)">Edit</button>
+      <button @click="deleteTemplate(template.id)">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      templates: []
+    };
+  },
+  methods: {
+    editTemplate(id) {
+      // Logic to edit the template
+    },
+    deleteTemplate(id) {
+      // Logic to delete the template
+    }
+  }
+};
+</script>
