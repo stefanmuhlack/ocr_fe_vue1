f863bb81949fb57bb3ae207e48aa72a4acf0186b
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-18 12:59:25 +0200

    Update App.vue to include navigation and routing for the new components.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,47 @@
 <template>
   <div id="app">
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/upload">Upload</router-link></li>
+        <li><router-link to="/templates">Templates</router-link></li>
+      </ul>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+nav li {
+  display: inline;
+  margin-right: 10px;
+}
+nav a {
+  text-decoration: none;
+  color: #42b983;
+}
+</style>
+
+<template>
+  <div id="app">
     <header>
       <h1>OCR Application</h1>
 <template>
