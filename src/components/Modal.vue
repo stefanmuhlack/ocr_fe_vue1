7bb8bbf5b59d4817d96d8118a559880e8f3e1672
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-22 14:48:41 +0200

    Improved accessibility and added close button functionality in Modal component.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,36 +1,58 @@
 <template>
-  <transition name="fade">
-    <div v-if="visible" class="modal-overlay" @click.self="close">
-      <div class="modal-content">
-        <slot></slot>
-      </div>
+  <div class="modal" v-if="isVisible" role="dialog" aria-modal="true">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <slot></slot>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
-
-export default defineComponent({
+export default {
   name: 'Modal',
   props: {
-    visible: Boolean
+    isVisible: {
+      type: Boolean,
+      required: true
+    }
   },
-  emits: ['update:visible'],
-  setup(props, { emit }) {
-    const close = () => {
-      emit('update:visible', false);
-    };
-
-    return { close };
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    }
   }
-});
+};
 </script>
 
 <style scoped>
-.modal-overlay {
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal-content {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  width: 50%;
+  max-width: 600px;
+}
+.close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
+
   left: 0;
   right: 0;
   bottom: 0;
