dde8e152ec3bd772899466e510bd2aa690c49870
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-25 17:52:09 +0200

    Implemented basic structure for ThumbnailPreview.vue.

--- src/components/ThumbnailPreview.vue
+++ src/components/ThumbnailPreview.vue
@@ -1,11 +1,32 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 
+const thumbnails = ref([]);
+
+onMounted(() => {
+  // Logic to fetch and display thumbnails
+});
 </script>
 
 <template>
-
+  <div class="thumbnail-preview">
+    <div v-for="(thumbnail, index) in thumbnails" :key="index">
+      <img :src="thumbnail.src" :alt="thumbnail.alt" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.thumbnail-preview {
+  display: flex;
+  flex-wrap: wrap;
+}
 
+.thumbnail-preview img {
+  margin: 5px;
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+}
 </style>
+
